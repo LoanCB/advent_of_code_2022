b84a8d89258f059b1be7c26c6ee3e6fc277b0f11
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2022-12-04 21:28:50 +0100

    day 1 exercice 1

--- day_1/ex_1.py
+++ day_1/ex_1.py
@@ -1,5 +1,13 @@
-file = open('input_1.txt')
-lines = file.readlines()
+data = list(map(lambda s: s.strip(), open('input_1.txt').readlines()))
+result = 0
+temp = 0
 
-for row in lines:
-    print(row)
+for i in data:
+    if not i:
+        temp = 0
+        continue
+    temp += int(i)
+    if temp > result:
+        result = temp
+
+print(result)
